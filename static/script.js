7902353d6b2923fff34ded1919798d3f04f0833b
PAULO HENRIQUE GAMA <paulohg_pool@hotmail.com>
2021-06-18 20:29:31 -0300

    update
    Feito o tratamento de alguns erros, como selecionar mais de um personagem ou não selecionar nenhum.

--- static/script.js
+++ static/script.js
@@ -1,5 +1,6 @@
 let arma_selecionada,personagem_selecionado;
-let list_arma_selecionada = []
+let list_arma_selecionada = [] 
+let list_personagem_selecionado = []
 
 const personagens = document.getElementsByClassName("personagens")
 
@@ -20,6 +21,7 @@ function selecionarPersonagem(evento){
     if (!personagemSelecionado.classList.contains("selecionadoPersonagem")){
         personagemSelecionado.classList.add("selecionadoPersonagem");
         personagemSelecionado.style.borderTop = "5px solid black";
+        list_personagem_selecionado.push(nomePersonagem)
         
         const texto_Personagem = document.getElementById("text_personagem").innerHTML = `Você selecionou o(a) ${nomePersonagem}`
         
@@ -27,9 +29,26 @@ function selecionarPersonagem(evento){
     }else{
         personagemSelecionado.classList.remove("selecionadoPersonagem");
         personagemSelecionado.style.borderTop = "none";
+        let posicao = list_personagem_selecionado.indexOf(nomePersonagem)
+        console.log("posicaçao é: " + posicao)
+        if (posicao > -1){
+            list_personagem_selecionado.splice(posicao,1)
+
+        }
 
     }
-    personagem_selecionado = nomePersonagem;
+    if(list_personagem_selecionado.length===0){
+        const texto_personagem = document.getElementById("text_personagem").innerHTML = 'Selecione um personagem'
+
+    }else{
+        if(list_personagem_selecionado.length>1){
+            alert("Por favor deixa apenas um personagem selecionado")
+        }else{    
+        const texto_personagem = document.getElementById("text_personagem").innerHTML = `Você selecionou o(a) ${list_personagem_selecionado[0]}`
+        
+        }
+    }
+    personagem_selecionado = list_personagem_selecionado[0]
 }
 
 const armas = document.getElementsByClassName("armas")
@@ -43,28 +62,31 @@ function selecionarArma(evento){
     const nomeArma = armaSelecionada.getAttribute("id")
     
     
-    
     if(!armaSelecionada.classList.contains("selecionadoArma")){
         armaSelecionada.classList.add("selecionadoArma");
         armaSelecionada.style.border = "2px solid black"
-        arma_selecionada = nomeArma
-        list_arma_selecionada.push(nomeArma)
         
+        list_arma_selecionada.push(nomeArma)
+        arma_selecionada = list_arma_selecionada[0]
         
         
     }else{
         armaSelecionada.classList.remove("selecionadoArma");
         armaSelecionada.style.border = "none"
         let posicao = list_arma_selecionada.indexOf(nomeArma)
-        console.log("posicaçao é: " + posicao)
+        
         if (posicao > -1){
             list_arma_selecionada.splice(posicao,1)
 
         }
+        console.log("lista arma tem valor = "+ list_arma_selecionada[0])
+        //Verifica se tem alguma arma selecionada
+        if(list_arma_selecionada[0] === undefined){
+            alert("Selecione pelo menos uma arma!!")
+        }
     }
     const armasSelecionadas = document.querySelectorAll(".selecionadoArma")
 
-    console.log(list_arma_selecionada.length)
     if(list_arma_selecionada.length===0){
         const texto_arma = document.getElementById("text_arma").innerHTML = 'Selecione uma arma'
 
@@ -85,7 +107,7 @@ function calcularDano(){
     let nivel, nome_arma;
     nome_arma = arma_selecionada;
     let texto_dano,vida;
-    
+    console.log("arma é " + nome_arma)
     if (nome_arma === "soco"){
         nivel = 13
     }else{
@@ -102,7 +124,7 @@ function calcularDano(){
     
 
     dano = Math.floor(Math.random() * nivel) + 1;
-
+    console.log("personagem é " + personagem_selecionado)
     if(personagem_selecionado === "orc"){
         vida = vida_personagem.Orc
     }else{
@@ -113,12 +135,22 @@ function calcularDano(){
             vida = vida_personagem.Feiticeiro
         }
     }
-
-    if (dano >= vida){
-        texto_dano = document.getElementById("text_dano").innerHTML ="Você causou " +  dano + " de dano e o seu oponente está morto"
+    console.log("vida é " + vida)
+    if(list_arma_selecionada[0] === undefined){
+        alert("Selecione pelo menos uma arma!!")
+        texto_dano = document.getElementById("text_dano").innerHTML ="Você precisa selecionar uma arma."
     }else{
-        texto_dano = document.getElementById("text_dano").innerHTML ="Você causou " +  dano + " de dano e o seu oponente está vivo"
+        if(personagem_selecionado === undefined){
+            alert("Selecione um personagem!!")
+            texto_dano = document.getElementById("text_dano").innerHTML ="Por favor selecione um personagem!"
+        }else{
+            if (dano >= vida){
+                texto_dano = document.getElementById("text_dano").innerHTML ="Você causou " +  dano + " de dano e o seu oponente está morto"
+            }else{
+                texto_dano = document.getElementById("text_dano").innerHTML ="Você causou " +  dano + " de dano e o seu oponente está vivo"
 
+            }
+        }
     }
 
     
